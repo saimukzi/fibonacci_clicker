be39fea63307d3e767089cd61b1708e8b120ca67
saimukzi <saimukzi@proton.me>
2022-06-07 12:06:04 +0800

    beforeDestroy > beforeUnmount

--- vue/src/fc/FcMain.vue
+++ vue/src/fc/FcMain.vue
@@ -27,7 +27,7 @@ export default {
     this.fcSave.loadAutoSave();
     this.tickTimer = setInterval(this.tick, 1000/60);
   },
-  beforeDestroy() {
+  beforeUnmount() {
     clearInterval(this.tickTimer);
   },
 }
